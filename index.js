8813c376f1c552e5030371540becd98ac6e2218b
Roshan Melvin G A <74945888+roshan-melvin@users.noreply.github.com>
2026-06-26 07:57:05 +0000

    modified

--- index.js
+++ index.js
@@ -5,3 +5,10 @@ function greet() {
   console.log("Welcome to the GitHub hidden features guide.");
 }
 greet();
+
+// This will trigger a CodeQL alert on your PR
+function runVulnerableCode(userInput) {
+    // CodeQL tracks untrusted input entering this execution sink
+    eval(userInput); 
+}
+runVulnerableCode("console.log('Testing CodeQL');");
