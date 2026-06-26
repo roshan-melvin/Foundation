e0f319e7dc489458877e06bd168adf153bbae8d1
Roshan Melvin G A <74945888+roshan-melvin@users.noreply.github.com>
2026-06-26 06:49:54 +0000

    Sample js added

---
+++ index.js
@@ -0,0 +1,9 @@
+// Placeholder file to initialize CodeQL Analysis
+function greetUser(name) {
+  if (!name) {
+    return "Hello, World!";
+  }
+  return `Hello, ${name}!`;
+}
+
+greetUser("Developer");
